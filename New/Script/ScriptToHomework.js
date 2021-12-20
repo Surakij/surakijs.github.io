54ab178ee6a94df475ff50ee5920292f176d7093
Sergey <71408120+Surakij@users.noreply.github.com>
2021-12-20 22:13:33 +0400

    Update jS

--- New/Script/ScriptToHomework.js
+++ New/Script/ScriptToHomework.js
@@ -1,33 +1,42 @@
 const button = document.querySelector(".button");
 button.hidden = true;
 
-var text = getInputText();
-
 const input = document.querySelector(".input_field");
 
 input.addEventListener("click", showButton);
 
-button.addEventListener("click", addElementToList(text));
-
+button.addEventListener("click", addElementToList);
+//вызов кнопки и меняем значение спрятать на противоположное
 function showButton() {
   const button = document.querySelector(".button");
   button.hidden = false;
-
-  //button.addEventListener('click', removeButton);
 }
+//вызов кнопки и ее скрытие
 function removeButton() {
   const button = document.querySelector(".button");
   button.hidden = true;
 }
 
+//обращение к полю ввода и возвращение его значения (введенный текст)
 function getInputText() {
   var input = document.querySelector(".input_field");
   return input.value;
 }
-function addElementToList(text) {
-  const listItem = document.createElement("p");
+//по клику кнопки будет выполняться:
 
+function addElementToList() {
+  //забираем текст из поля ввода
+  const text = getInputText();
+  //создаем новый элемент и записываем в него значение из поля ввода
+  const listItem = document.createElement("p");
+  listItem.innerText = text;
+  //обращаюсь в DIV по классу и добавляем в него новый элемент
   const list = document.querySelector(".list");
   list.append(listItem);
-  listItem.innerText = text;
+  //обращаемся к кнопке и скрываем кнопку
+  const button = document.querySelector(".button");
+  button.addEventListener("click", removeButton);
+  // очистка поля ввода
+  const input = document.querySelector(".input_field");
+  input.value = "";
 }
