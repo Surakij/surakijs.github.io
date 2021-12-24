d25421be9c82905e19ea20ef99113130b489b53b
Sergey <71408120+Surakij@users.noreply.github.com>
2021-12-24 10:36:59 +0400

    Update_script_task2
    code formating

--- HomeWork8/task2/Script_task2/script_task2.js
+++ HomeWork8/task2/Script_task2/script_task2.js
@@ -3,6 +3,6 @@ const date = new Date();
 const hrs = date.getHours();
 const mins = date.getMinutes();
 
-const PassMins = (hrs*60)+mins;
+const PassMins = hrs * 60 + mins;
 
 console.log(PassMins);
